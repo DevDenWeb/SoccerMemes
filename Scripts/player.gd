90ec26e31cec0c169b7381834c6e7d7f7243da7b
Denis <nosovdenis14@gmail.com>
2025-01-30 14:10:23 +0300

    added a "for" loop

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -36,6 +36,7 @@ extends CharacterBody2D
 var speed: float = 300
 var jump_force: float = -500
 var gravity: float = 980
+var push_force = 300
 
 func _physics_process(delta):
 	var horizontal_input = Input.get_axis("ui_left", "ui_right")
@@ -46,5 +47,12 @@ func _physics_process(delta):
 	
 	if Input.is_action_just_pressed("ui_up") and is_on_floor():
 		velocity.y = jump_force
-
+	
 	move_and_slide()
+	
+	for i in get_slide_collision_count():
+		var c = get_slide_collision(i)
+		if c.get_collider().name == "Ball":
+			c.get_collider().apply_central_impulse(-c.get_normal() * push_force)
+			print("yes")
+	
